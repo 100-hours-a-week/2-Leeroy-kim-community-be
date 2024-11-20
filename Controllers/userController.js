246f050891e3ebd085a6bdde7a706ba9826b0389
Dwisgolmog <rlaehdgus134@naver.com>
2024-11-21 02:45:48 +0900

    Feat: 로그인 & 회원가입 로직 완성

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,11 +1,78 @@
 const userModel = require('../Models/userModel');
 
-exports.login = async (req,res)=>{
-    const loginUser = await userModel.loginUser();
-    res.status(200).json({ message:'로그인 성공!' , data: loginUser });
-}
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
 
-exports.signup = async(req,res)=>{
-    const signUp = await userModel.addUser();
-    res.status(201).json({message:'회원가입 성공!', data: signUp});
-}
+    if (!email && !password) {
+        return res
+            .status(400)
+            .json({ message: '입력한 값이 비어있습니다.', data: null });
+    }
+
+    try {
+        const result = await userModel.loginUser(email, password);
+
+        if (result == 404)
+            return res.status(404).json({
+                message: '존재하지 않는 이메일 입니다.',
+                data: null,
+            });
+
+        if (result == 400)
+            return res.status(400).json({
+                message: '비밀번호가 틀렸습니다.',
+                data: null,
+            });
+
+        return res.status(200).json({
+            message: '로그인 성공!',
+            data: {
+                user_id: result.user_id,
+                nickname: result.nickname,
+            },
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            message: '서버에서 에러가 발생했습니다!',
+            data: null,
+        });
+    }
+};
+
+exports.signup = async (req, res) => {
+    const { email, password, nickname, profile_img } = req.body;
+
+    if (!email && !password && !nickname)
+        return res
+            .status(400)
+            .json({ message: '입력한 값이 비어있습니다.', data: null });
+
+    try {
+        const result = await userModel.addUser(
+            email,
+            password,
+            nickname,
+            profile_img
+        );
+
+        if (result == 400)
+            return res.status(400).json({
+                message: '이메일이 중복되었습니다.',
+                data: null,
+            });
+
+        return res.status(201).json({
+            message: '회원가입이 완료 되었습니다!',
+            data: {
+                user_id: result,
+            },
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            message: '서버에서 에러가 발생했습니다!',
+            data: null,
+        });
+    }
+};
--- Models/userModel.js
+++ Models/userModel.js
@@ -1,9 +1,66 @@
-exports.addUser = async() =>{
-    const result = "유저 회원가입 성공!"
-    return result;
-}
+const crypto = require('crypto');
+const path = require('path');
+const fs = require('fs').promises;
+const dataPath = path.join(__dirname, '../data/userInfo.json');
 
-exports.loginUser = async() =>{
-    const result = "유저 로그인 성공!";
-    return result;
-}
+//NOTE: 로그인 로직
+exports.loginUser = async (email, password) => {
+    try {
+        const data = await fs.readFile(dataPath, 'utf8');
+        const userData = JSON.parse(data);
+
+        const user = userData.users.find((user) => user.email === email);
+
+        if (!user) return 404;
+
+        const inputPassword = await crypto
+            .createHash('sha256')
+            .update(password + user.salt)
+            .digest('hex');
+
+        if (inputPassword === user.password) {
+            return { user_id: user.user_id, nickname: user.nickname };
+        } else {
+            return 400;
+        }
+    } catch (e) {
+        console.log(`로그인 로직 에러 발생 => ${e}`);
+        throw new Error('로그인중 문제가 발생했습니다!');
+    }
+};
+
+//NOTE: 회원가입 로직
+exports.addUser = async (email, password, nickname, profile_img) => {
+    try {
+        const data = await fs.readFile(dataPath, 'utf8');
+        const userData = JSON.parse(data);
+
+        //NOTE: 이메일 중복 확인
+        const user = userData.users.find((user) => user.email === email);
+        if (user) return 400;
+
+        const salt = await crypto.randomBytes(128).toString('base64');
+        password = await crypto
+            .createHash('sha256')
+            .update(password + salt)
+            .digest('hex');
+
+        const newUser = {
+            user_id: userData.users.length,
+            email: email,
+            password: password,
+            salt: salt,
+            nickname: nickname,
+            profile_img: profile_img,
+        };
+
+        userData.users.push(newUser);
+        await fs.writeFile(dataPath, JSON.stringify(userData, null, 4), 'utf8');
+
+        //NOTE: 유저id 반환
+        return userData.users.length - 1;
+    } catch (e) {
+        console.log(`회원가입중 에러 발생 => ${e}`);
+        throw new Error('회원가입중 문제가 발생했습니다!');
+    }
+};
