4a9a4d69b4be8d38f26aa5ab0fb500a0c2c15835
Dwisgolmog <rlaehdgus134@naver.com>
2024-12-19 16:44:09 +0900

    Fix & Chore: RateLimit 값 조정 & .gitignore 파일 업데이트

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ app.use(
 app.use(timeout('5s'));
 //api 요청 제한 미들웨어
 exports.apiLimiter = RateLimit({
-    windowMs: 60 * 10000, //1분
+    windowMs: 60 * 1000, //1분
     max: 50,
     handler(req, res) {
         res.status(this.statusCode).json({
             code: this.statusCode, //RateLimit의 반환객체는 429code를 default로 반환하게 되어있음
-            message: '1분에 5번만 요청 할 수 있습니다.',
+            message: '1분에 50번만 요청 할 수 있습니다.',
         });
     },
 });
