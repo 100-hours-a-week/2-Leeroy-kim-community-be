55c3403216bbf86bb8a8fa74ebfe06038f4cb599
Dwisgolmog <rlaehdgus134@naver.com>
2025-01-07 11:29:07 +0900

    Fix:댓글 및 게시글 날짜 출력 수정
    기존에 2022-05-24T10:30:25+09:00 와 같이 날짜가 나오던것을 2022년 05월 24일 10:30:25 로 출력되게 수정하였음
    commentModel에서 불필요한 코드 삭제

--- Models/boardModel.js
+++ Models/boardModel.js
@@ -68,6 +68,17 @@ exports.getBoard = async (board_id) => {
             view_count: boardRows[0].view_count + 1,
             comment_count: boardRows[0].comment_count,
             user_id: boardRows[0].user_id,
+            board_date: boardRows[0].update_date
+                ? dayjs(boardRows[0].update_date).format(
+                      'YYYY년 MM월 DD일 HH:mm:ss'
+                  )
+                : dayjs(boardRows[0].board_date).format(
+                      'YYYY년 MM월 DD일 HH:mm:ss'
+                  ),
+            like_count: boardRows[0].like_count,
+            view_count: boardRows[0].view_count + 1,
+            comment_count: boardRows[0].comment_count,
+            user_id: boardRows[0].user_id,
             nickname: userRows[0].nickname,
             profile_img:
                 userRows[0].profile_img != null
@@ -184,6 +195,9 @@ exports.getBoardList = async (page, limit) => {
         const result = rows.map((row) => {
             return {
                 ...row,
+                board_date: row.update_date
+                    ? dayjs(row.update_date).format('YYYY년 MM월 DD일 HH:mm:ss')
+                    : dayjs(row.board_date).format('YYYY년 MM월 DD일 HH:mm:ss'),
                 profile_img:
                     row.profile_img != null
                         ? `http://${process.env.BACKEND_URL}:5050${row.profile_img}`
